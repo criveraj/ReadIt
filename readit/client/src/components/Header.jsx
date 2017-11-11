f27c0e2f3073341540c5f19a13a93dc2d72ffeab
dluu0630 <davidluu0630@gmail.com>
2017-11-11 13:30:52 -0800

    Added authentication

---
+++ readit/client/src/components/Header.jsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Auth from '../modules/Auth';
+import {Redirect} from 'react-router-dom';
+
+
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // set the initial component state
+    this.state = {
+      SiteText: 'Readit',
+      loginText: 'Log In',
+      signupText: 'Sign up',
+      userWelcomeText: 'Hello ',
+      redirect: false,
+    };
+
+    this.onLogOutClicked = this.onLogOutClicked.bind(this);
+    
+  }    
+
+  onLogOutClicked()
+  {
+    Auth.deauthenticateUser();
+    
+  }
+
+  render() {
+    return (
+
+
+      
+
+    <div className="top-bar">
+
+      
+
+        <div className="top-bar-left">
+                <Link to="/">{this.state.SiteText}</Link>   
+        </div>
+
+
+        <div>
+        {Auth.isUserAuthenticated() == true ? (
+
+            <div className="top-bar-left">
+
+                  {this.state.userWelcomeText} {JSON.parse(localStorage.getItem('usrname')).name}!
+
+            </div>  
+            ):
+            (
+
+            <div>
+            </div>  
+
+            )}
+
+        </div>
+
+        <div className="top-bar-left">
+            {/* <button onClick={this.onLanguageSettingClicked}> */}
+              <Link to="/" onClick={this.onLanguageSettingClicked}>{this.state.LangBtnText}</Link>   
+
+              {/* {this.state.LangBtnText} */}
+            {/* </button> */}
+        </div>  
+
+        {/* <div className="top-bar-left" onClick={this.onLanguageSettingClicked}>
+            中文版
+        </div>       */}
+
+
+        {Auth.isUserAuthenticated() == false ? (
+        <div className="top-bar-right">
+                 <Link to="/login">{this.state.loginText}</Link> 
+                 <Link to="/signup">{this.state.signupText}</Link>   
+        </div>
+        ):  
+        (
+        <div className="top-bar-right">
+                 <Link to="/login" onClick={this.onLogOutClicked}>Log out</Link> 
+        </div>
+        )
+        }
+
+    </div>
+    
+    );
+  }
+}
+
+export default Header
---
+++ readit/client/src/components/Main.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router-dom'
+import Home from './Home.jsx'
+import LoginPage from '../containers/LoginPage.jsx';
+import SignUpPage from '../containers/SignUpPage.jsx';
+import Auth from '../modules/Auth';
+
+
+class Main extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentGreeting: localStorage.getItem('currentGreeting'),
+      // localStorage.
+      
+    };
+
+  }
+  
+  render() {
+    var title = localStorage.getItem('currentGreeting') == 'Website title';
+    var subtitle = localStorage.getItem('currentLanguage') == 'Eng' ? 
+    'This is the home page.':
+    '这里是主页，请登录查看更多内容';
+    // console.log();
+
+    // let homeRoute = <Home cardtitleP={localStorage.getItem('currentLanguage')} cardsubtitleP={localStorage.getItem('currentLanguage')}/>;
+    // let homeRoute = <Route exact path='/' render={() => <Home cardtitleP={localStorage.getItem('currentLanguage')} cardsubtitleP={localStorage.getItem('currentLanguage')}/>}/>;   
+
+    return (
+          <main>
+
+            <Switch>
+                 {/* <Route exact path='/' component={Home}/>    */}
+              {/* <Route exact path='/' render={() => <Home cardtitleP="Website Title" cardsubtitleP="This is the home page. Log in to see the hidden content"/>}/> */}
+                  <Route exact path='/' render={() => <Home cardtitleP={title} cardsubtitleP={subtitle}/>}/>    
+                {/* <Route exact path='/' render={renderTarget}/>   */}
+              {/* {homeRoute} */}
+
+              {/* <Router path="/somePath" render={() => <SomeComponent someProp={prop} />} />       */}
+              {/* <Route path='/roster' component={Roster}/>
+              <Route path='/schedule' component={Schedule}/> */}
+
+              {/* {Auth.isUserAuthenticated() ? (
+              <div>    
+              <Route path='/login' component={LoginPage}/>
+              </div>      
+              ):
+              (
+              <div>    
+              <Route path='/login' component={LoginPage}/>
+              <Route path='/signup' component={SignUpPage}/>
+              </div>      
+              )
+
+              } */}
+              <Route path='/login' component={LoginPage}/>
+              <Route path='/signup' component={SignUpPage}/>
+
+            </Switch>
+
+          </main>
+    )
+  }  
+}
+
+export default Main
