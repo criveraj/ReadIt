517f91015fba32a7d8fd3d3009cc111d02f1bf89
dluu0630 <davidluu0630@gmail.com>
2017-11-11 13:20:46 -0800

    cleaned up notes

--- readit/server/models/index.js
+++ readit/server/models/index.js
@@ -6,16 +6,6 @@ module.exports.connect = (uri) => {
     process.exit(1);
   });
 
-//   mongoose.connect(uri);
-  // plug in the promise library:
-//   mongoose.Promise = global.Promise;
-
-
-//   mongoose.connection.on('error', (err) => {
-//     console.error(`Mongoose connection error: ${err}`);
-//     process.exit(1);
-//   });
-
   // load models
   require('./user');
 };
